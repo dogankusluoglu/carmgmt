344fa4a6008d09aefd19d2e89abe63f1f807229f
dogie <dkusluoglu@gmail.com>
2024-04-07 00:22:20 +0200

    date and carBrand working and fixed

--- carbackend/carmanager/models.py
+++ carbackend/carmanager/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, blank=True, default="")
+    carBrand = models.CharField(max_length=100, blank=True, default="")
     year = models.CharField(max_length=4, blank=True, default="")
     model = models.CharField(max_length=100, blank=True, default="")
     colour = models.CharField(max_length=12, blank=True, default="")
@@ -10,7 +11,7 @@ class Car(models.Model):
     reg = models.CharField(max_length=20, blank=True, default="")
     cost = models.CharField(max_length=6, blank=True, default="")
     retail = models.CharField(max_length=6, blank=True, default="")
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
