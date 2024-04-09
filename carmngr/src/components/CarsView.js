87f0bec274b9f4535d622424bb516e6249ebb3a8
dogie <dkusluoglu@gmail.com>
2024-04-09 20:55:31 +0200

    Replaced Car View With Better Data table

--- carmngr/src/components/CarsView.js
+++ carmngr/src/components/CarsView.js
@@ -5,13 +5,17 @@ import useSWR from "swr";
 
 import dayjs from 'dayjs';
 
-import { CollapsibleTable } from "./CollapsibleTable";
+import MUIDataTable from "mui-datatables";
+
+// import { CollapsibleTable } from "./CollapsibleTable";
 
 export const CarsView = () => {
 
     // Using useSWR hook to fetch data
-    // const { data:cars, error, isLoading } = useSWR('/cars', fetcher);
-    // console.log(error)
+    const { data:cars, error, isLoading } = useSWR('/cars', fetcher);
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+    console.log(JSON.stringify(cars))
 
     // Header Setup (for later edit vehicle)
     const username = 'dogie';
@@ -22,10 +26,57 @@ export const CarsView = () => {
     headers.append('Authorization', `Basic ${encodedCredentials}`);
     headers.append('Content-Type', 'application/json');
 
+    const columns = [
+        "Year", 
+        "Brand", 
+        "Model", 
+        "Colour",
+        "Mileage", 
+        "Purchased From", 
+        "VIN Number", 
+        "Registration",
+        "Cost Price", 
+        "Retail Price", 
+        "Date Purchased"
+    ];
+
+    const data = cars.map(car => [
+        car.year, 
+        car.carBrand, 
+        car.model, 
+        car.colour,
+        car.mileage,
+        car.purchasedFrom,
+        car.vin,
+        car.reg,
+        car.cost,
+        car.retail,
+        car.date,
+        // In case I need to stringify the expenses array as well
+        // JSON.stringify(car.expenses),
+        // car.totalSpent,
+        // car.salesman,
+        // car.soldDate,
+        // car.profit,
+        // car.soldTo,
+        // car.notes,
+        // car.status
+    ])
+
+    const options = {
+        filterType: 'checkbox',
+    };
+
     return (
         <div>
             <h1>View All Cars</h1>
-            <CollapsibleTable />
+            {/* <CollapsibleTable /> */}
+            <MUIDataTable
+                title={"Car List"}
+                data={data}
+                columns={columns}
+                options={options}
+            />
         </div>
     )
 }
