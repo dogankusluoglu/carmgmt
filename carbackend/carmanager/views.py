8bc1371bd1b8d1c4f4d22df7cbe375aa3b59552a
dogie <dkusluoglu@gmail.com>
2024-04-11 11:00:56 +0200

    delete an expense from the table

--- carbackend/carmanager/views.py
+++ carbackend/carmanager/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics, permissions, viewsets
+from django.http import JsonResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+
+from rest_framework import generics, permissions, viewsets, status
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -25,6 +29,32 @@ class ExpenseList(generics.ListCreateAPIView):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
 
+
 class ExpenseDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Car.objects.all()
-    serializer_class = CarSerializer
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+    """
+    Retrieve, update or delete an expense instance.
+    """
+    # def get_object(self, pk):
+    #     try:
+    #         return Expense.objects.get(pk=pk)
+    #     except Expense.DoesNotExist:
+    #         return Response({'message': 'Expense not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # def get(self, request, pk, format=None):
+    #     expense = self.get_object(pk)
+    #     if isinstance(expense, Response):
+    #         return expense  # Early return if the expense was not found
+    #     serializer = ExpenseSerializer(expense)
+    #     return Response(serializer.data)
+    
+
+    def delete_expense(request, id):
+        if request.method == 'DELETE':
+            expense = get_object_or_404(Expense, id=id)
+            expense.delete()
+            return JsonResponse({'message': 'Expense deleted successfully!'}, status=204)
+        else:
+            return HttpResponseNotAllowed(['DELETE'])
--- carmngr/src/components/Expenses.js
+++ carmngr/src/components/Expenses.js
@@ -10,25 +10,61 @@ export const Expenses = () => {
     
     const navigate = useNavigate()
 
+    // const [ expensesData, setExpensesData ] = useState("")
+    
     const { data:cars, errorCars, isLoadingCars } = useSWR(`/cars`, fetcher);
     const { data:expenses, error, isLoading } = useSWR(`/expenses`, fetcher);
+    // useEffect(() => {
+    // if(expenses) setExpensesData(expenses);
+    //     }, [expenses]);
     if (error || errorCars) return <div>failed to load</div>
     if (isLoading || isLoadingCars) return <div>loading...</div>
     // console.log(expenses)
     // console.log(cars)
 
-    const carData = cars.map(car => [
+    // Header Setup (for later edit vehicle)
+    const username = 'dogie';
+    const password = 'dogie';
+    const headers = new Headers();
+    // Encode username and password base64
+    const encodedCredentials = btoa(`${username}:${password}`);
+    headers.append('Authorization', `Basic ${encodedCredentials}`);
+    headers.append('Content-Type', 'application/json');
+
+    const carData = cars ? cars.map(car => [
         car.id,
         car.year,
         car.carBrand,
         car.model,
         car.colour
-    ])
+    ]) : []
 
-    const carLookup = cars.reduce((acc, car) => {
+    const carLookup = cars ? cars.reduce((acc, car) => {
         acc[car.id] = car.year + " " + car.carBrand + " " + car.model + " (" + car.colour + ")"; 
         return acc;
-      }, {});
+      }, {}) : "";
+
+    const deleteRow = async (id) => {
+        try {
+            const response = await fetchx(`/expenses/${id}`,{
+                method: 'DELETE',
+                headers: headers
+            });
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            }
+    
+            // Check if there's content to parse
+            let data = 'Delete successful';
+            if (response.status !== 204) { // 204 No Content
+                data = await response.json(); // Only parse JSON if there's content
+            }
+            
+            console.log(data); // Log the success message or returned data
+        } catch (err) {
+            console.error('Error deleting the record:', err);
+        }
+    }
 
     const columnsExp = [
         "ID",
@@ -38,18 +74,28 @@ export const Expenses = () => {
         "Car"
     ];
 
-    const expensesData = expenses.map(expense => [
+    const expensesData = expenses ? expenses.map(expense => [
         expense.id,
         expense.description,
         expense.amount,
         expense.created_at,
         carLookup[expense.car] || 'Unknown Car'
-    ])
+    ]): []
 
     const options = {
         filterType: 'checkbox',
         onRowClick: (rowData, rowMeta) => {
             navigate(`/expenses/${rowData[0]}`);
+        },
+        onRowsDelete: (rowsDeleted, dataRows) => {
+            const dataIndexes = rowsDeleted.data.map(row => row.dataIndex);
+            dataIndexes.forEach(index => {
+                // Assuming the ID is in the first column of your data
+                const id = expensesData[index][0];
+                // console.log(`ID of the deleted row: ${id}`);
+                deleteRow(id)
+            });
+            // console.log(rowsDeleted);
         }
     };
 
